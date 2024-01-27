ad621f6c951a20ad06e0409d9ade55ff3ee99221
Kousheek Chakraborty <kousheekc@gmail.com>
2024-01-27 12:23:02 +0100

    joint control works

--- milling_robot_kinematics/milling_robot_kinematics/control_node.py
+++ milling_robot_kinematics/milling_robot_kinematics/control_node.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
 from .ui_file import Ui_MainWindow
 
+import math
 import threading
 
 import rclpy
@@ -18,24 +19,31 @@ class ControlPanel(QMainWindow):
 
         self.ui.q1_minus.clicked.connect(self.q1_minus_click)
         self.ui.q1_plus.clicked.connect(self.q1_plus_click)
+        self.ui.q2_minus.clicked.connect(self.q2_minus_click)
+        self.ui.q2_plus.clicked.connect(self.q2_plus_click)
+        self.ui.q3_minus.clicked.connect(self.q3_minus_click)
+        self.ui.q3_plus.clicked.connect(self.q3_plus_click)
+        self.ui.d4_minus.clicked.connect(self.d4_minus_click)
+        self.ui.d4_plus.clicked.connect(self.d4_plus_click)
 
         self.current_joint_state = {
-            "q1": 0.0,
-            "q2": 0.0,
-            "q3": 0.0,
-            "d4": 0.0
+            'Q1': 0.0,
+            'Q2': 0.0,
+            'Q3': 0.0,
+            'D4': 0.0
         }
 
         self.current_cartesian_state = {
-            "x": 0.0,
-            "y": 0.0,
-            "z": 0.0
+            'X': 0.0,
+            'Y': 0.0,
+            'Z': 0.0
         }
 
         rclpy.init()
         self.node = Node("control_panel_node")
 
         self.joint_state_pub = self.node.create_publisher(JointState, 'joint_states', 10)
+        self.joint_state_timer = self.node.create_timer(0.1, self.joint_state_pub_callback)
 
         self.ros_thread = threading.Thread(target=self.run_ros_node)
         self.ros_thread.daemon = True
@@ -48,25 +56,37 @@ class ControlPanel(QMainWindow):
         except KeyboardInterrupt:
             pass
 
-    def generate_joint_state(self, delta):
+    def joint_state_pub_callback(self):
         msg = JointState()
         msg.header.stamp = self.node.get_clock().now().to_msg()
-        for i, (joint, val) in enumerate(self.current_joint_state.items()):
-            new_val = val + delta[i]
-            self.current_joint_state[joint] = new_val
+        for joint, val in self.current_joint_state.items():
             msg.name.append(joint)
-            msg.position.append(new_val)
-        return msg
+            msg.position.append(val)
+        self.joint_state_pub.publish(msg)
 
     def q1_minus_click(self):
-        print("q1 minus pressed")
-        msg = self.generate_joint_state([-self.ui.q1_inc.value(), 0, 0, 0])
-        self.joint_state_pub.publish(msg)
+        self.current_joint_state['Q1'] -= math.radians(self.ui.q1_inc.value())
 
     def q1_plus_click(self):
-        print("q1 plus pressed")
-        msg = self.generate_joint_state([self.ui.q1_inc.value(), 0, 0, 0])
-        self.joint_state_pub.publish(msg)
+        self.current_joint_state['Q1'] += math.radians(self.ui.q1_inc.value())
+
+    def q2_minus_click(self):
+        self.current_joint_state['Q2'] -= math.radians(self.ui.q2_inc.value())
+
+    def q2_plus_click(self):
+        self.current_joint_state['Q2'] += math.radians(self.ui.q2_inc.value())
+
+    def q3_minus_click(self):
+        self.current_joint_state['Q3'] -= math.radians(self.ui.q3_inc.value())
+
+    def q3_plus_click(self):
+        self.current_joint_state['Q3'] += math.radians(self.ui.q3_inc.value())
+
+    def d4_minus_click(self):
+        self.current_joint_state['D4'] -= self.ui.d4_inc.value() / 1000.0
+
+    def d4_plus_click(self):
+        self.current_joint_state['D4'] += self.ui.d4_inc.value() / 1000.0
 
 def main():
     app = QApplication([])
